775b8063bc0a1f4567ea1d878000064c1ab09c2a
Edwin <74559064+edwinrm98@users.noreply.github.com>
2025-05-07 10:54:00 -0600

    Maquetación del footer

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<main class="main">
-    <article class="articles">
-        <ul class="articles--list">
-            <a href="#" class="articles--list-link"><li class="articles--list-text">Titulo de la entrada</li></a>
-            <a href="#" class="articles--list-link"><li class="articles--list-text">Titulo de la entrada</li></a>
-            <a href="#" class="articles--list-link"><li class="articles--list-text">Titulo de la entrada</li></a>
-            <a href="#" class="articles--list-link"><li class="articles--list-text">Titulo de la entrada</li></a>
-        </ul>
-    </article>
-</main>
+    <main class="main">
+        <article class="articles">
+            <ul class="articles--list">
+                <a href="#" class="articles--list-link"><li class="articles--list-text">Titulo de la entrada</li></a>
+                <a href="#" class="articles--list-link"><li class="articles--list-text">Titulo de la entrada</li></a>
+                <a href="#" class="articles--list-link"><li class="articles--list-text">Titulo de la entrada</li></a>
+                <a href="#" class="articles--list-link"><li class="articles--list-text">Titulo de la entrada</li></a>
+            </ul>
+        </article>
+    </main>
+</body>
